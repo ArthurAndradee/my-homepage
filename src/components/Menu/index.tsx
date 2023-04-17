17da9916b1478f5d5b6cbe78b115ff155b12054d
arthur andrade <arthurandrad.23@gmail.com>
2023-04-17 20:58:56 -0300

    dont know what to name this one

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -3,10 +3,12 @@ import React from 'react';
 import { Section, Options, MainText, } from "./styles"
 
 const Menu: React.FC = () => {
-  const [showText, setShowText] = React.useState(false)
+  const [showProject, setShowProject] = React.useState(false)
+  const [showInfo, setShowInfo] = React.useState(false)
+  const [showContact, setShowContact] = React.useState(false)
 
   const Projects = () => (
-    <div 
+    <section 
     id="Text" 
     className="search-Text"
     style={{
@@ -25,41 +27,97 @@ const Menu: React.FC = () => {
       color: "var(--black)",
       
     }}
-    onMouseEnter={() => setShowText(true)} 
-    onMouseLeave={() => setShowText(false)}
+    onMouseEnter={() => setShowProject(true)} 
+    onMouseLeave={() => setShowProject(false)}
     >
       ‎ Aqui  você  verá  alguns  dos  projetos  os  quais  tenho  
       trabalhado  nos  últimos  meses,  para  mais  detalhes  clique  no  
       ícone  do  Github.
-      <br></br><br></br>
-      • Lista de Tarefas em typescript<br></br><br></br>
-      • Aplicativo de conversão de temperatura<br></br><br></br>
-      • Clone do twitter<br></br><br></br>
-      • Jogo de pedra, papel e tesoura<br></br><br></br>
-    </div>
+                                                <br/><br/>
+      • Lista de Tarefas em typescript          <br/><br/>
+      • Aplicativo de conversão de temperatura  <br/><br/>
+      • Clone do twitter                        <br/><br/>
+      • Jogo de pedra, papel e tesoura          <br/><br/>
+    </section>
+  )
+
+  const Info = () => (
+    <section 
+    id="Text" 
+    className="search-Text"
+    style={{
+      width: "700px", 
+
+      marginRight: "200px",
+      marginTop: "25px",
+
+      fontFamily: "'Open Sans', sans-serif",
+      fontWeight: 'bold',
+      fontSize: "18px",
+
+      textAlign: "left",
+      letterSpacing: "2px",
+
+      color: "var(--black)",
+      
+    }}
+    onMouseEnter={() => setShowInfo(true)} 
+    onMouseLeave={() => setShowInfo(false)}
+    >
+      AAAAAA
+    </section>
+  )
+
+  const Contact = () => (
+    <section 
+    id="Text" 
+    className="search-Text"
+    style={{
+      width: "700px", 
+
+      marginRight: "200px",
+      marginTop: "25px",
+
+      fontFamily: "'Open Sans', sans-serif",
+      fontWeight: 'bold',
+      fontSize: "18px",
+
+      textAlign: "left",
+      letterSpacing: "2px",
+
+      color: "var(--black)",
+      
+    }}
+    onMouseEnter={() => setShowContact(true)} 
+    onMouseLeave={() => setShowContact(false)}
+    >
+      STAYING ALIVE
+    </section>
   )
 
   return (
       <div>
         <Options>
           <Section
-            onMouseEnter={() => setShowText(true)} 
-            onMouseLeave={() => setShowText(false)}>
+            onMouseEnter={() => setShowProject(true)} 
+            onMouseLeave={() => setShowProject(false)}>
               Projects
           </Section>
           <Section
-            onMouseEnter={() => setShowText(true)} 
-            onMouseLeave={() => setShowText(false)}>
+            onMouseEnter={() => setShowInfo(true)} 
+            onMouseLeave={() => setShowInfo(false)}>
               Info
           </Section>
           <Section
-            onMouseEnter={() => setShowText(true)} 
-            onMouseLeave={() => setShowText(false)}>
+            onMouseEnter={() => setShowContact(true)} 
+            onMouseLeave={() => setShowContact(false)}>
               Contact
           </Section>
         </Options>
         <MainText>
-          { showText ? <Projects /> : null }
+          { showProject ? <Projects /> : null }
+          { showInfo ? <Info /> : null }
+          { showContact ? <Contact /> : null }
         </MainText>
     </div>
     )
